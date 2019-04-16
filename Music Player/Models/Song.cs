58445761a2e9c7299eb9dd28e8f4a10132fc8d29
Bartosz Grochal <radu480@o2.pl>
2019-04-16 18:35:41 +0200

    Add Song class to Model.

---
+++ Music Player/Models/Song.cs
@@ -0,0 +1,26 @@
+﻿using Music_Player.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_Player.Models
+{
+    public class Song : ObservableObject
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { OnPropertyChanged(ref _name, value); }
+        }
+
+        private string _artist;
+        public string Artist
+        {
+            get { return _artist; }
+            set { OnPropertyChanged(ref _artist, value); }
+        }
+    }
+}
