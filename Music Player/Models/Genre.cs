24063c966243d5ec383789da11749fc6b16d17d6
Bartosz Grochal <radu480@o2.pl>
2019-04-17 12:24:49 +0200

    Add Genre enumeration to model.

---
+++ Music Player/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_Player.Models
+{
+    public enum Genre
+    {
+        Metal,Rock,Pop,Rap,Punk,Grunge,Blues,Country
+    }
+}
