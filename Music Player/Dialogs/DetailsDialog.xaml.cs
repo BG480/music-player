8b5c6bb18ab26f097392c5cd36a822e6b9eb3ef3
Bartosz Grochal <radu480@o2.pl>
2019-04-19 16:58:10 +0200

    Add custom Song Details dialog.

---
+++ Music Player/Dialogs/DetailsDialog.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Music_Player.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Music_Player.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for DetailsDialog.xaml
+    /// </summary>
+    public partial class DetailsDialog : Window
+    {
+        public DetailsDialog(string name="", string artist="", Genre genre=Genre.Metal)
+        {
+            InitializeComponent();
+            cmbGenre.ItemsSource = Enum.GetValues(typeof(Genre)).Cast<Genre>();
+            txtName.Text = name;
+            txtArtist.Text = artist;
+            cmbGenre.SelectedIndex = (int)genre;
+        }
+
+        public string SongName
+        {
+            get { return txtName.Text;  }
+        }
+
+        public string SongArtist
+        {
+            get { return txtArtist.Text; }
+        }
+
+        public Genre SongGenre
+        {
+            get { return (Genre)Enum.Parse(typeof(Genre), cmbGenre.SelectedValue.ToString()); }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
