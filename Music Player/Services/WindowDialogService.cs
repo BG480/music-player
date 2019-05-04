72094b7ad388dd27134b804dfacc901053b6d8e9
Bartosz Grochal <radu480@o2.pl>
2019-05-04 12:57:14 +0200

    Fix OpenFileDialog filter.

--- Music Player/Services/WindowDialogService.cs
+++ Music Player/Services/WindowDialogService.cs
@@ -14,6 +14,7 @@ namespace Music_Player.Services
         public string OpenFile(string filter)
         {
             var dialog = new OpenFileDialog();
+            dialog.Filter = filter;
 
             if (dialog.ShowDialog() == true)
             {
